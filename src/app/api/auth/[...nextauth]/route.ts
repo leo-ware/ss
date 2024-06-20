408ade3985e2d39d427559c3b1beb4c442ff8302
Leo Ware <leoware@uni.minerva.edu>
2024-06-20 12:10:00 -0400

    before supabase

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "username and password",
+            credentials: {
+                username: { label: "Username", type: "text", paceholder: "username" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize (credentials, req) {
+                return { id: "123", name: "Test User"}
+            }
+        })
+    ]
+})
+
+export { handler as GET, handler as POST }
